7fb89282b4bde1e1cea80cc543fe83f8d5d0ca21
osuvak <suvako@gmail.com>
2018-05-12 16:35:28 +0300

    license info added to recent ex files

--- src/src_deploy/content/stuff/compiled/tests/simple/source/tutorials/callbacks/c_lib_interfacing_with_cpp/static_method_in_abstract_father_class/callbacks/father.hh
+++ src/src_deploy/content/stuff/compiled/tests/simple/source/tutorials/callbacks/c_lib_interfacing_with_cpp/static_method_in_abstract_father_class/callbacks/father.hh
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the "dev-in-place" repository located at:
+ * https://github.com/osuvak/dev-in-place
+ * 
+ * Copyright (C) 2018  Onder Suvak
+ * 
+ * For licensing information check the above url.
+ * Please do not remove this header.
+ * */
+
 #ifndef CALLBACKS_FATHER_HH_
 #define CALLBACKS_FATHER_HH_
 

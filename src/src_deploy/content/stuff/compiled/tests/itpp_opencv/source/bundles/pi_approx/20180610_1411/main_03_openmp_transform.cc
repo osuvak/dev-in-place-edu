8997a968105d1e1b7638eacf42d937a84828378e
osuvak <suvako@gmail.com>
2018-06-11 16:20:12 +0300

    added missing copyright info headers

--- src/src_deploy/content/stuff/compiled/tests/itpp_opencv/source/bundles/pi_approx/20180610_1411/main_03_openmp_transform.cc
+++ src/src_deploy/content/stuff/compiled/tests/itpp_opencv/source/bundles/pi_approx/20180610_1411/main_03_openmp_transform.cc
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the "dev-in-place" repository located at:
+ * https://github.com/osuvak/dev-in-place
+ * 
+ * Copyright (C) 2018  Onder Suvak
+ * 
+ * For licensing information check the above url.
+ * Please do not remove this header.
+ * */
+
 #include <itpp/itbase.h>
 #include <vector>
 #include <algorithm>

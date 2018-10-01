ae64349ee6d3407075aaf59c47426c22a811f132
osuvak <suvako@gmail.com>
2018-10-01 13:57:52 +0300

    rm core lib dep - opencv

--- src/src_deploy/content/lib_info/OpSu132_b32_deploy/types/core_os_nc.mk
+++ src/src_deploy/content/lib_info/OpSu132_b32_deploy/types/core_os_nc.mk
@@ -18,7 +18,6 @@ PATH_LINK:=
 DIR_ABS_LIBS := $(HOME)/dev_in_place_content/lib_info/$(DIR_LIB_INFO_CURRENT)/libs
 
 include $(DIR_ABS_LIBS)/config_lib_itpp_04.03.01_20170302_1603.mk
-include $(DIR_ABS_LIBS)/config_lib_opencv_3.2.0_20170302_1500.mk
 include $(DIR_ABS_LIBS)/config_lib_fftw_03.03.04_20170302_1541.mk
 include $(DIR_ABS_LIBS)/config_lib_acml_04_04_00_gfortran_32bit.mk
 # include $(DIR_ABS_LIBS)/.mk
